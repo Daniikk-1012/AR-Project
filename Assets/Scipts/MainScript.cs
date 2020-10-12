f6f53f1ad06621bcff45c8b0cfe32854c84bd91d
Daniikk-1012 <daniyarsar@gmail.com>
2020-10-12 11:54:25 +0600

    Basic working application
    First working version with UI and models of skeleton and cardio vascular system

---
+++ Assets/Scipts/MainScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.SimpleLocalization;
+using Vuforia;
+
+public class MainScript : MonoBehaviour
+{
+    private const string defaultLayer = "Skeleton";
+    private const string defaultSex = "Male";
+
+    public Text text;
+
+    private GameObject selectedObject;
+    private GameObject selectedLayer;
+    private GameObject selectedSex;
+
+    public void Awake() {
+        LocalizationManager.Read("localization.csv");
+        switch (Application.systemLanguage) {
+            default:
+                LocalizationManager.Language = "English";
+                break;
+        }
+        SelectSex(defaultSex);
+    }
+
+    public void SelectObject(GameObject select) {
+        ResetSelectedObject();
+        selectedObject = select;
+        Material[] materials;
+        materials = selectedObject.GetComponent<MeshRenderer>().materials;
+        foreach (Material material in materials) {
+            material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+        }
+        text.text = LocalizationManager.Localize(selectedObject.name);
+    }
+
+    public void ResetSelectedObject() {
+        if (selectedObject != null) {
+            Material[] materials = selectedObject.GetComponent<MeshRenderer>().materials;
+            foreach (Material material in materials) {
+                material.shader = Shader.Find("Diffuse");
+            }
+        }
+    }
+
+    public void SelectLayer(string layerName) {
+        if(selectedLayer != null) {
+            selectedLayer.SetActive(false);
+        }
+        foreach(Transform childTransform in selectedSex.GetComponentsInChildren<Transform>(true)) {
+            if(childTransform.gameObject.name == layerName) {
+                selectedLayer = childTransform.gameObject;
+                selectedLayer.SetActive(true);
+            }
+        }
+        ResetSelectedObject();
+    }
+
+    public void SelectSex(string sexName) {
+        if(selectedSex != null) {
+            selectedSex.SetActive(false);
+        }
+        foreach(Transform childTransform in FindObjectOfType<ImageTargetBehaviour>().GetComponentsInChildren<Transform>(true)) {
+            if(childTransform.gameObject.name == sexName) {
+                selectedSex = childTransform.gameObject;
+                selectedSex.SetActive(true);
+            }
+        }
+        SelectLayer(defaultLayer);
+    }
+}
---
+++ Assets/Scipts/RaycastModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.SimpleLocalization;
+
+public class RaycastModel : MonoBehaviour{
+    public MainScript mainScript;
+
+    void Update() {
+        foreach(Touch touch in Input.touches) {
+            if (touch.fingerId == 0) {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                if (touch.phase == TouchPhase.Began) {
+                    RaycastHit raycastHit = new RaycastHit();
+                    Physics.Raycast(ray, out raycastHit);
+                    if (raycastHit.collider.gameObject == gameObject) {
+                        mainScript.SelectObject(gameObject);
+                    }
+                }
+            }
+        }
+    }
+}
